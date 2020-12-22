32831e0e050da13070624d00c4fa314b8b9773ef
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2020-12-22 17:11:46 +0000

    update

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ def main():
     ce = CE('coreexome_map')
     ce.add_file() # if file is present already (to avoid running table_dump() every time during development)
     ce.step('chip_comp',0,20)
+#    ce.step('chip_comp',0)
 
 if __name__ == "__main__":
     main()
