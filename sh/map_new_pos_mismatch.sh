890d3e5b271df1bfb644b5e959a7a50d324cbf65
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-01-06 17:08:43 +0000

    send output to file

--- sh/map_new_pos_mismatch.sh
+++ sh/map_new_pos_mismatch.sh
@@ -2,14 +2,17 @@
 new_pos_mismatch=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 crawl="${DIR}/rs_crawl.sh"
+myname=`basename "$0"`
+myname=${myname/map_/}
+outfile="out_${myname%.*}_rs.txt"
 while IFS=$'\t' read -r nothing id tabloc dblocs
 do
     id=${id%% *}
 #    dbloc=${dblocs%/*}
     if [[ $id =~ ^rs[0-9] ]]; then
         idlookup=$($crawl $id)
-        echo -e "$id\ttab=$tabloc\tdb=$dblocs\tlookup=$idlookup"
+        echo -e "$id\ttab=$tabloc\tdb=$dblocs\tlookup=$idlookup" | tee -a $outfile
     else
-        echo -e "$id\ttab=$tabloc\tdb=$dblocs"
+        echo -e "$id\ttab=$tabloc\tdb=$dblocs" | tee -a $outfile
     fi
 done < $new_pos_mismatch
--- sh/map_no38pos.sh
+++ sh/map_no38pos.sh
@@ -2,13 +2,16 @@
 no38pos=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 crawl="${DIR}/rs_crawl.sh"
+myname=`basename "$0"`
+myname=${myname/map/out}
+outfile="${myname%.*}_rs.txt"
 while IFS=$'\t' read -r nothing id pos
 do
     id=${id%% *}
     if [[ $id =~ ^rs[0-9] ]]; then
         idlookup=$($crawl $id)
-        echo -e "$id\t$pos\tlookup:$idlookup"
+        echo -e "$id\t$pos\tlookup:$idlookup" | tee -a $outfile
     else
-        echo -e "$id\t$pos"
+        echo -e "$id\t$pos" | tee -a $outfile
     fi
 done < $no38pos
