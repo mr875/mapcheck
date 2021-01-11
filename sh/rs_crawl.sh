31f489f7ffb13f583bdfb3e0d25024c5af02efe6
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-01-11 10:58:05 +0000

    refine temp file name for when script might be run in parallel

--- sh/rs_crawl.sh
+++ sh/rs_crawl.sh
@@ -4,7 +4,9 @@ baseurl='https://www.ncbi.nlm.nih.gov/snp/?term='
 altbase='https://www.ncbi.nlm.nih.gov/snp/'
 rsid=$1
 url=$baseurl$rsid
-out='out_rs_crawl'
+ending=$(date +%s)
+ending="${ending: -5}"
+out="out_rs_crawl_$ending"
 wget "$url" -q -O $out 
 grep "<title>Search: $rsid - NCBI</title>" $out > /dev/null
 if [ $? -eq 0 ]; then
@@ -17,7 +19,7 @@ if [ $? -eq 0 ]; then
 fi
 chrpos38=$(grep "</a></span></dd><dt>Chromosome: </dt><dd>" $out | sed 's/^.\+<dd>//' | uniq | paste -s -d ' ')
 chrpos37=$(grep '(GRCh38)<br />' $out | sed 's/.\+\/>//' | uniq | paste -s -d ' ')
-merges=$(egrep -o "rs[0-9]+ has merged into .+rs[0-9]+\">" out_rs_crawl | sed 's/^\(rs[0-9]\+\).\+\(rs[0-9]\+\).*$/\1 \2/' | paste -s -d ',')
+merges=$(egrep -o "rs[0-9]+ has merged into .+rs[0-9]+\">" $out | sed 's/^\(rs[0-9]\+\).\+\(rs[0-9]\+\).*$/\1 \2/' | paste -s -d ',')
 if [ -z "$merges" ]; then
     echo -e "${rsid}\tb38=$chrpos38\tb37=$chrpos37"
 else
