bf8615280a0bedaa65c5e378530ede17d605e61c
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-02-08 15:45:01 +0000

    edits

--- sh/map_new_rs_byalt.sh
+++ sh/map_new_rs_byalt.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-new_rs=$1
-inpname=$(basename $new_rs)
+rs_byalt=$1
+inpname=$(basename $rs_byalt)
 inpname=${inpname%%_*}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 crawl="${DIR}/rs_crawl.sh"
 myname=`basename "$0"`
 myname=${myname/map_/}
 outfile="$(date +%s)_${inpname}_out_${myname%.*}_rs.txt"
-while IFS=$'\t' read -r newrs secid
+while IFS=$'\t' read -r newrs linkid mid
 do
 #    ori=${id#*orig };   ori=${ori%)}
     newrs=${newrs##* }
-    secid=${secid##* }
+    linkid=${linkid##* }
+    mid=${mid##* }
     idlookup=$($crawl $newrs)
-    echo -e "map table rs= $newrs\tlink id= $secid\tlookup= $idlookup" | tee -a $outfile
-done < $new_rs
+    echo -e "map table rs= $newrs\tlink id= $linkid\tomics mid= $mid\tlookup= $idlookup" | tee -a $outfile
+done < $rs_byalt
