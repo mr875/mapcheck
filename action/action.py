b9804038dd50c3ef1bb9a49829cb874ae2cfd15f
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-02-16 17:30:07 +0000

    progress back up

--- action/action.py
+++ action/action.py
@@ -17,7 +17,7 @@ class ProcFile(NewAltRs_05,NewPosMisM_06,NewRs_07):
         self.tabname = tabname
         self.reportmode = reportmode
         self.make_extra_map_table()
-        brk=5 # set to 0 for whole file # if 20 then line number 19 gets done, line 20 does not
+        brk=20 # set to 0 for whole file # if 20 then line number 19 gets done, line 20 does not
         start=0 # set to 0 for no action. if 10 then line number 10 gets done
         self.dbact_om = open('dbact_om_' + self.ts + '_' + tabname + '.sql',"w")
         self.dbact_br = open('dbact_br_' + self.ts + '_' + tabname + '.sql',"w")
--- action/newrs_07.py
+++ action/newrs_07.py
@@ -4,7 +4,7 @@ import re
 # py fileproc.py out_files_corexome/out_sh/out_new_rs_rs.txt coreexome_map
 
 class NewRs_07:
-#map table rsid not known to db, map table non-rsid in db consensus instead
+# map table rsid not known to db, map table non-rsid in db consensus instead
 # related (newrsbyalt): map table rsid not known to db, map table non-rsid in db alt_ids instead
     def newrs(self,brk=0,start=0):
         report = open('report_newrs_' + self.ts + '_' + self.tabname + '.txt',"w")
@@ -26,13 +26,32 @@ class NewRs_07:
             if becomes:
                 print('map table id %s should be merged into %s. No action yet' % (mapid,'/'.join(becomes)))
                 continue
-#            if not self.stillmain(linkid):
-#                print('link id %s is not in consensus table. Would have swapped it out with %s from map table' % (linkid,mapid))
-#                continue
-            b38 = self.getb38(new_current[1])
-            getbrpos = self.checkbr_pos(linkid,b38)
-            if getbrpos[0]:
-                print('swapping main id %s out of omics consensus table and new id %s from map table in' % (linkid,mapid))
-                self.swapout_main(swin=mapid,swout=linkid,ds=self.tabname)               
+            if not self.stillmain(linkid):
+                print('link id %s is not in consensus table. Would have swapped it out with %s from map table' % (linkid,mapid))
                 continue
-            print('no action ',mapid,linkid,b38,getbrpos)
+            b38 = self.getb38(new_current[1])
+            if not b38:
+                print('b38 position of map table id %s (omics %s) not available (may be withdrawn)' % (mapid,linkid))
+                continue
+            contig = False
+            if '_' in b38:
+                print('map table id %s (omics %s) dbsnp look up has a contig id instead of regular chromosome identifier: %s. Will swap %s into omics for %s but will not correct/update positions' % (mapid,linkid,b38,mapid,linkid))
+                contig = True
+            getbrpos = self.checkbr_pos(mapid,b38)
+            getompos = self.checkom_pos(linkid,chrpos=b38,build='38')
+            if not contig:
+                if not getbrpos[0]:
+                    oldbrpos = [op for op in getbrpos[1] if op != '0:0']
+                    for op in oldbrpos:
+                        report.write('map table id %s to be swapped in to consensus table in place of %s. %s in map table does not match dbsnp coordinate (%s vs %s). Correcting position in map table\n' % (mapid,linkid,mapid,op,b38))
+                        self.mtab_change_pos(anid=mapid,oldpos=op,newpos=b38)
+                if not getompos[0]:
+                    report.write('map table id %s to be swapped in to consensus table in place of %s. %s in omics does not match dbsnp coordinate (%s vs %s). Adding dbsnp coordinate %s to omics\n' % (mapid,linkid,linkid,b38,','.join(getompos[1]),b38))
+                    badom = [bp for bp in getompos[1] if bp != '0:0']
+                    self.addpos(linkid,chrpos=b38,ds='dbsnp',build='38')
+                    for bp in badom:
+                        print('flagging position %s in omics db because we have a dbsnp sourced coordinate %s for this variant (%s/%s)' % (bp,b38,linkid,mapid))
+                        self.pos_flag(linkid,chrpos=bp,fl=-5)
+            report.write('swapping main id %s out of omics consensus table and new id %s from map table in\n' % (linkid,mapid))
+            self.swapout_main(swin=mapid,swout=linkid,ds=self.tabname)               
+        print('Done. %s seconds' % (int(timeit.default_timer() - self.grandstart)))
