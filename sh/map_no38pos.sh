74cd70a70a9a6a2c26a96171faa8147309e9ab6a
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-02-19 16:53:16 +0000

    update

--- sh/map_no38pos.sh
+++ sh/map_no38pos.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 no38pos=$1
+inpname=$(basename $no38pos)
+inpname=${inpname%%_*}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 crawl="${DIR}/rs_crawl.sh"
 myname=`basename "$0"`
-myname=${myname/map/out}
-outfile="${myname%.*}_rs.txt"
+myname=${myname/map_/}
+#outfile="${myname%.*}_rs.txt"
+outfile="$(date +%s)_${inpname}_out_${myname%.*}_rs.txt"
 while IFS=$'\t' read -r nothing id pos
 do
+    ori=${id##* }
+    ori=${ori%)}
     id=${id%% *}
     if [[ $id =~ ^rs[0-9] ]]; then
         idlookup=$($crawl $id)
-        echo -e "$id\t$pos\tlookup:$idlookup" | tee -a $outfile
+        echo -e "$id\t$pos\tbrorig=$ori\tlookup:$idlookup" | tee -a $outfile
     else
-        echo -e "$id\t$pos" | tee -a $outfile
+        echo -e "$id\t$pos\tbrorig=$ori" | tee -a $outfile
     fi
 done < $no38pos
