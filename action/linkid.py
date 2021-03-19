ed941c0de296dbf107da3b74c4eac268e7e1a3ff
marc.rossello <marc.rossello@bioresource.nihr.ac.uk>
2021-03-19 17:38:39 +0000

    progess backup

--- action/linkid.py
+++ action/linkid.py
@@ -1,3 +1,61 @@
 
+
+dsmap = {'170':['coreexome_map'],'171':['coreexome_map'],'232':['coreexome_map'],'69':['humanexome_map'],'233':['humanexome_map'],'70':['infiniumimmunoarray_map'],'234':['infiniumimmunoarray_map'],'72':['msexome_map'],'73':['omniexpress_map','omniexpress_v2_1_map'],'235':['omniexpress_map','omniexpress_v2_1_map'],'71':['ukbbaffy_map'],'231':['ukbbaffy_v2_1_map'],'999':[]} 
+# NOTE ds 999 has no map table equivalent
+
+def uidcol(maptab):
+    uidcols = {'ukbbaffy_v2_1_map':'chipid'}
+    return uidcols.get(maptab,'snp') # even for ukbbaffy_map, although its snp == v2_1.snp, no chipid
+
+def rscol(maptab):
+    rscols = {}
+    return rscols.get(maptab,'dbsnpid')
+
 class LinkID:
-    pass
+    
+    @classmethod
+    def get_flank_ds(cls,mid,omcurs):
+        qry = 'SELECT datasource, chosen FROM flank WHERE id = %s'
+        omcurs.execute(qry,(mid,))
+        ls = omcurs.fetchall()
+        return ls
+
+    @classmethod
+    def anotds_to_mapt(cls,ds):
+        return dsmap.get(ds,ds) # default = ds, because if it's not in the dict then the ds is probably a map table itself
+
+    @classmethod
+    def where_col(cls,mid,maptable,brcurs):
+        mt_col = uidcol(maptable)
+        rs_col = rscol(maptable)
+        where = ' WHERE ' + rs_col + ' REGEXP %s'
+        val = (mid+'[[:>:]]',)
+        colwithid = rs_col
+        res = None
+        if 'rs' in mid:
+            q = 'SELECT ' + mt_col + ',' + rs_col + ',chr FROM ' + maptable + where
+            brcurs.execute(q,val)
+            res = brcurs.fetchall()
+        if not res:
+            where = ' WHERE ' + mt_col + ' = %s'
+            val = (mid,)
+            q = 'SELECT ' + mt_col + ',' + rs_col + ',chr FROM ' + maptable + where
+            brcurs.execute(q,val)
+            res = brcurs.fetchall()
+            colwithid = mt_col
+        return [where,val,colwithid,res] # check if val in not None to detect success
+
+    @classmethod
+    def get_maptab_where(cls,mid,maptable,brcurs,omcurs=None):
+        resarr = cls.where_col(mid,maptable,brcurs)
+        if not resarr[3] and omcurs:
+            q = 'SELECT alt_id FROM alt_ids WHERE id = %s'
+            val = (mid,)
+            omcurs.execute(q,val)
+            alts = omcurs.fetchall()
+            alts = [r[0] for r in alts]
+            for alt in alts:
+                resarr = cls.where_col(alt,maptable,brcurs)
+                if resarr[3]:
+                    break
+        return resarr
--- flankerr.py
+++ flankerr.py
@@ -13,6 +13,7 @@ def loadConf():
 class FlankErr: 
     
     qry = 'select distinct id from flank where id in (select id from flank group by id having sum(chosen) > 0)'
+    qfname = 'flankerr_out.txt'
 
     def __init__(self, argv):
         dbmap = loadConf()
@@ -20,16 +21,44 @@ class FlankErr:
         self.omcurs = omics.getCursor()
         br = DBConnect(dbmap['br'])
         self.brcurs = br.getCursor()
-        queryout = QueryFile('flankerr_out.txt',self.qry,db=dbmap['omics'])
-        self.action_file(queryout)
-        omics.close()
-        br.close()
+        queryout = QueryFile(self.qfname,self.qry,db=dbmap['omics'])
+        try:
+            self.action_file(queryout)
+        finally:
+            omics.close()
+            br.close()
 
     def action_file(self,queryout):
         cnt = 0
         for mid in queryout.read():
             cnt += 1
-            if cnt > 9:
+            allds = LinkID.get_flank_ds(mid,self.omcurs)
+            nchose_ds = [var[0] for var in allds if var[1] == 0]
+            mapts = []
+            for ds in nchose_ds:
+                if ds == '114':
+                    print('datasource DIL Taqman (114) for %s ignored' % (mid))
+                    continue
+                if ds == 'ext':
+                    raise Exception('datasource %s for %s should not have a flank error\n' % (ds,mid))
+                mapts.extend(LinkID.anotds_to_mapt(ds))
+            #print(mapts)
+            mapwhere = []
+            mapval = []
+            mapts_fltrd = []
+            for mt in mapts:
+                where,val,colwithid,res = LinkID.get_maptab_where(mid,mt,self.brcurs,self.omcurs)
+                if res:
+                    mapts_fltrd.append(mt)
+                    mapwhere.append(where)
+                    mapval.append(val)
+            #print(mapts_fltrd,mapwhere,mapval)
+            if not len(mapwhere) == len(mapval) == len(mapts_fltrd):
+                raise Exception('lists mapwhere, mapval, mapts_fltrd should be the same size',mapwhere, mapval,mapts_fltrd)
+            for ind,where in enumerate(mapwhere):
+                full = 'UPDATE ' + mapts_fltrd[ind] + ' SET ... ' + where 
+                print(full % mapval[ind])
+            if cnt > 3:
                 break
 
 def main(argv):
